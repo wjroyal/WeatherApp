e7933d56684aed381ebf9a1a0769afd999b37b2d
Will Royal <william.j.royal@gmail.com>
2024-10-24 23:43:41 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
